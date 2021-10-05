8a948f6a05099f7caedd281825a0fb0ee724b0f6
robin <robinnagpal.tiet@gmail.com>
2021-10-05 16:40:11 -0400

    Update ts config

--- 01-basic-hardhat-setup/hardhat.config.ts
+++ 01-basic-hardhat-setup/hardhat.config.ts
@@ -1,17 +1,24 @@
-import {HardhatUserConfig} from 'hardhat/types';
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
+import { node_url, accounts } from "./utils/network";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import { HardhatUserConfig } from "hardhat/types";
 
 const config: HardhatUserConfig = {
-    solidity: {
-        version: '0.8.6',
+  solidity: {
+    version: "0.8.6",
+  },
+  namedAccounts: {
+    deployer: "0x470579d16401a36BF63b1428eaA7189FBdE5Fee9",
+    tokenOwner: 1,
+  },
+  networks: {
+    ropsten: {
+      url: node_url("ropsten"),
+      accounts: accounts("ropsten"),
     },
-    namedAccounts: {
-        deployer: 0,
-        tokenOwner: 1,
-    },
-    paths: {
-        sources: "src"
-    }
+  },
+  paths: {
+    sources: "src",
+  },
 };
 export default config;
